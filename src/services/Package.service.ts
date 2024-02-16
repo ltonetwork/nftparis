75912eafa336c7ec94254aa692bbdace8e858731
colStie <stiemsdev@gmail.com>
2024-02-16 11:19:53 +0100

    update

--- src/services/Package.service.ts
+++ src/services/Package.service.ts
@@ -135,7 +135,7 @@ export default class PackageService {
     const name: string = packageJson.name || zipFile.name.replace(/\.\w+$/, '');
     const title = name
       .replace(/^ownable-|-ownable$/, '')
-      .replace(/[-_]+/, ' ')
+      .replaceAll(/[-_]+/, ' ')
       .replace(/\b\w/, c => c.toUpperCase());
     const description: string|undefined = packageJson.description;
 
